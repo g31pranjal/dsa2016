e07e579dfb26dcaeb16c04eefd6ecbfa6a42ffeb
Pranjal <g31.pranjal@gmail.com>
2016-04-16 16:31:57 +0530

    update

--- dsa/bst.c
+++ dsa/bst.c
@@ -39,23 +39,101 @@ node * insertNode(node * root, int val) {
 	}
 }
 
-int findMax(node * root) {
+
+node * findMax(node * root) {
 	if(root != NULL) {
 		if(root->right == NULL)
-			return root->key;
+			return root;
 		else 
 			findMax(root->right);
 	}
 }
 
-int findMin(node * root) {
+node * findMin(node * root) {
 	if(root != NULL) {
 		if(root->left == NULL)
-			return root->key;
+			return root;
 		else 
-			findMax(root->left);
+			findMin(root->left);
 	}
 }
+
+node * searchNode(node * root, int val) {
+	node * result;
+
+	if(root == NULL) {
+		return NULL;
+	}
+	else {
+		if(root->key == val) {
+			return root;
+		}
+		else if(root->key > val) {
+			if(root->left == NULL)
+				return NULL;
+			else {
+				result = searchNode(root->left, val);
+			}
+		}
+		else {
+			if(root->right == NULL)
+				return NULL;
+			else {
+				result = searchNode(root->right, val);
+			}	
+		}
+		return result;
+	}
+}
+
+node * transplant(node * root, node * u, node *v) {
+	
+	if(u->parent == NULL) {
+		root = v;
+	}
+	else {
+		if(u->parent->left == u) 
+			u->parent->left = v;
+		else 
+			u->parent->right = v;
+		
+		if(v != NULL) {
+			v->parent = u->parent;
+		}
+	}
+
+	return root;
+}
+
+
+node * deleteNode(node * root, int val) {
+	node * z = searchNode(root, val);
+	node * y;
+
+	if(z != NULL) {
+		if(z->left == NULL) {
+			root = transplant(root, z, z->right);
+		}
+		else if(z->right == NULL) {
+			root = transplant(root, z, z->left);
+		}
+		else {
+			y = findMin(z->right);
+			if(y->parent != z) {
+				root = transplant(root, y, y->right);
+				y->right = z->right;
+				z->right->parent = y;
+			}
+			root = transplant(root, z, y);
+			y->left = z->left;
+			z->left->parent = y;
+		}
+	}
+
+	return root;
+}
+
+
 
 void inorderTr(node * root) {
 	if(root->left != NULL)
@@ -77,6 +155,24 @@ int main() {
 	root = insertNode(root, 48);
 	root = insertNode(root, 50);
 
-	printf("min : %d\n", findMin(root));
+	inorderTr(root);
+	printf("\n");
+
+
+	root = deleteNode(root, 32);
+
+	inorderTr(root);
+	printf("\n");
+
+	root = deleteNode(root, 30);
+	root = deleteNode(root, 56);
+
+	inorderTr(root);
+
+
+	root = deleteNode(root, 48);
+	root = deleteNode(root, 50);
+
+
 }
 
---
+++ dsa/scc.c
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Stack stack;
+
+struct Stack {
+	int * arr;
+	int c;
+	int max;
+};
+
+stack * initStack(int max) {
+	stack * s = (stack *)malloc(sizeof(stack));
+	s->c = 0;
+	s->max = max;
+	s->arr = (int *)malloc(max*sizeof(int));
+
+	return s;
+}
+
+void push(stack *s, int n) {
+	if(s->c < s->max - 1) {
+		s->arr[s->c] = n;
+		s->c = s->c + 1;
+	}
+}
+
+int pop(stack * s) {
+	int t;
+	if(s->c != 0) {
+		t = s->arr[s->c-1];
+		s->c = s->c - 1;
+	}
+	return t;
+
+}
+
+
+
+typedef struct Node node;
+
+struct Node {
+	int v;
+	node * next;
+};
+
+typedef struct Graph graph;
+
+struct Graph {
+	int v;
+	int e;
+	node ** adj;
+	int * b;
+	int * f;
+	int * p;
+	int * s;
+};
+
+
+int tm;
+
+graph * initGraph(int num) {
+	graph * g = (graph *)malloc(sizeof(graph));
+	g->v = num;
+	g->e = 0;
+	g->adj = (node **)malloc(num*sizeof(node *));
+
+	g->b = (int *)malloc(num*sizeof(int));
+	g->f = (int *)malloc(num*sizeof(int));
+	g->p = (int *)malloc(num*sizeof(int));
+	g->s = (int *)malloc(num*sizeof(int));
+
+
+	int i=0;
+	for(i=0;i<num;i++) {
+		g->adj[i] = NULL;
+		g->s[i] = 0;
+		g->b[i] = 0;
+		g->f[i] = 0;
+		g->p[i] = -1;
+
+	}
+
+	return g;
+}
+
+void addEdge(graph * g, graph * gt, int u, int v) {
+	node * n = (node *)malloc(sizeof(node));
+	n->v = v;
+	n->next = g->adj[u];
+	g->adj[u] = n;
+
+	g->e = g->e + 1;
+	
+	n = (node *)malloc(sizeof(node));
+	n->v = u;
+	n->next = gt->adj[v];
+	gt->adj[v] = n;
+	
+	gt->e = gt->e + 1;
+}
+
+void printGraph(graph * g) {
+	int ver = g->v;
+	int i;
+	node * ele;
+	for(i=0;i<ver;i++) {
+		printf("%d :: ", i);	
+		ele = g->adj[i];
+		while(ele !=NULL) {
+			printf("%d --> ", ele->v);
+			ele = ele->next;
+		}
+		printf("\n");
+	}
+}
+
+
+typedef struct Edge edge;
+
+struct Edge {
+	int u;
+	int v;
+};
+
+edge e[50];
+int edgeC;
+
+
+void dfs(graph  *g, int s, stack *st){
+	tm++;
+	g->b[s] = tm;
+	g->s[s] = 1;
+	//printf("%d\t", s);
+
+	node *current= g->adj[s];
+	
+	while(current !=  NULL){
+		if(g->s[current->v] == 0){
+			g->p[current->v] = s;
+			
+			e[edgeC].u = s;
+			e[edgeC].v = current->v;
+			edgeC++;
+			
+			dfs(g, current->v, st);
+		}
+		current = current->next;
+	}
+
+	g->f[s]= ++tm;
+	g->s[s] = 2;
+	
+	push(st, s);
+}
+
+
+
+void scc(graph *g, graph *gt){
+	tm = 0;
+	int ver = g->v, i;
+	stack *st1= initStack(100);
+	stack *st2= initStack(100);
+
+	for(i=0;i<50;i++) {
+		e[i].u = -1;
+		e[i].v = -1;
+	}
+
+	edgeC = 0;
+
+	for(i=0;i<ver;i++){
+		if(g->s[i] == 0){
+			dfs(g, i, st1);
+			for(edgeC = edgeC - 1;edgeC>=0;edgeC--){
+				printf("(%d,%d)\t", e[edgeC].u , e[edgeC].v);
+			}
+			edgeC = 0;
+			printf("\n");
+		}
+	}
+
+	for(i=0;i<ver;i++) {
+		printf("%d :: %d\t%d\t%d\t%d\n", i, g->p[i], g->b[i], g->f[i], g->s[i] );
+	}
+
+	
+
+
+	edgeC = 0;
+	tm = 0;
+	
+	int vert;	
+
+	while(st1->c!=0){
+		vert= pop(st1);
+		if(gt->s[vert] == 0){
+			dfs(gt, vert, st2);
+			for(edgeC = edgeC - 1;edgeC>=0;edgeC--) {
+				printf("(%d,%d)\t", e[edgeC].u , e[edgeC].v);
+			}
+			edgeC = 0;
+			printf("\n");
+		}
+	}
+
+	for(i=0;i<ver;i++) {
+		printf("%d :: %d\t%d\t%d\t%d\n", i, gt->p[i], gt->b[i], gt->f[i], gt->s[i] );
+	}
+}
+
+
+
+int main(){
+	int ver = 8;
+	graph *g, * gt;
+	g = initGraph(ver);
+	gt= initGraph(ver);
+	
+	addEdge(g, gt, 0, 1);
+	addEdge(g, gt, 1, 2);
+	addEdge(g, gt, 2, 3);
+	addEdge(g, gt, 2, 5);
+	addEdge(g, gt, 3, 2);
+	addEdge(g, gt, 3, 4);
+	addEdge(g, gt, 4, 4);
+	addEdge(g, gt, 5, 4);
+	addEdge(g, gt, 6, 5);
+	addEdge(g, gt, 5, 6);
+	addEdge(g, gt, 7, 6);
+	addEdge(g, gt, 1, 6);
+	addEdge(g, gt, 1, 7);
+	addEdge(g, gt, 7, 0);
+
+
+	printGraph(g);
+	printf("\n\n");
+	printGraph(gt);
+
+	scc(g, gt);	
+}
